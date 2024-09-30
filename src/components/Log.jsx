e98104984753cd9585dfc94d1fe4ee1613f16ae1
XuanTiep <xuantiep2310@gmail.com>
2024-09-30 23:38:49 +0700

    first commit

---
+++ src/components/Log.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Log = ({turns}) => {
+  return <ol id='log'>
+    {turns.map((turn, index) => <li key={index}>{turn.player} selected {turn.square.row}, {turn.square.col}</li>)}
+  </ol>
+}
+
+export default Log
